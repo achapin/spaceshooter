257201d8c667c57c8a7e376ebfd38f29bd4c1fcd
Aaron <aaron.chapin@gmail.com>
2023-03-04 23:49:57 +0100

    Test to verify that ships can be destroyed as well

--- spaceshooter/Assets/Scripts/Damage/DamageTests/DamageTests.cs
+++ spaceshooter/Assets/Scripts/Damage/DamageTests/DamageTests.cs
@@ -13,20 +13,15 @@ namespace Damage.DamageTests
     {
         private const string testShipPath = "Assets/Prefabs/Testing/TestShip.prefab";
         private const string testTargetPath = "Assets/Prefabs/Testing/Targetbox.prefab";
+        private const string testTargetShipPath = "Assets/Prefabs/Testing/TestShipDamageable.prefab";
         private AsyncOperationHandle<GameObject> shipHandle;
         private AsyncOperationHandle<GameObject> targetHandle;
         private const float sixtyFPS = 1f / 60f;
 
-        [SetUp]
-        public void OnStart()
-        {
-            shipHandle = Addressables.InstantiateAsync(testShipPath);
-            targetHandle = Addressables.InstantiateAsync(testTargetPath);
-        }
-
         [UnityTest]
         public IEnumerator TargetTakesDamageAndIsDestroyed()
         {
+            targetHandle = Addressables.InstantiateAsync(testTargetPath);
             yield return targetHandle;
             var testTarget = targetHandle.Result;
             var target = testTarget.GetComponent<DamageableHandler>();
@@ -48,6 +43,46 @@ namespace Damage.DamageTests
         [UnityTest]
         public IEnumerator TargetTakesDamageFromWeaponAndIsDestroyed()
         {
+            shipHandle = Addressables.InstantiateAsync(testShipPath);
+            targetHandle = Addressables.InstantiateAsync(testTargetPath);
+            yield return targetHandle;
+            yield return shipHandle;
+            var testShip = shipHandle.Result;
+            var testTarget = targetHandle.Result;
+            testShip.transform.position = Vector3.back * 10f;
+            testShip.transform.forward = Vector3.forward;
+            testTarget.transform.position = Vector3.zero;
+            var ship = testShip.GetComponent<Ship>();
+            var target = testTarget.GetComponent<DamageableHandler>();
+            Assert.Greater(ship.PowerAllocated, 0);
+            float totalDamage = 0f;
+            bool wasDestroyed = false;
+            target.DamageTaken += delegate(float f) { totalDamage += f; };
+            target.Destroyed += delegate { wasDestroyed = true; };
+
+            var state = new InputState
+            {
+                isFiring = true,
+                increaseWeaponPower = true
+            };
+
+            yield return null;
+            for (var loop = 0; loop < 240; loop++)
+            {
+                ship.SetInputState(state);
+                ship.Update(sixtyFPS);
+            }
+            
+            Debug.Log($"Total damage {totalDamage} Was Destroyed? {wasDestroyed}");
+            Assert.Greater(totalDamage, 0f);
+            Assert.True(wasDestroyed);
+        }
+
+        [UnityTest]
+        public IEnumerator ShipCanBeDestroyed()
+        {
+            shipHandle = Addressables.InstantiateAsync(testShipPath);
+            targetHandle = Addressables.InstantiateAsync(testTargetShipPath);
             yield return targetHandle;
             yield return shipHandle;
             var testShip = shipHandle.Result;
